52cee5a05e0ec19f551e5cd6a92e178d930564d2
Emanuele Fumagalli <emanuelef@gmail.com>
2020-02-11 22:40:26 +0000

    Testing state in Agent

--- lib/myclient/main.ex
+++ lib/myclient/main.ex
@@ -5,22 +5,26 @@ defmodule Myclient.Main do
   @headers [Referer: "https://global.adsbexchange.com/VirtualRadar/desktop.html"]
 
   def start do
-    start_fetching()
+    {:ok, pid} = Stack.start_link()
+    start_fetching(pid)
   end
 
-  defp convert_to_map(json_text) do
+  defp convert_to_map(json_text, pid) do
     Myclient.Client.decode(json_text)
+    Stack.push(pid, :foo)
   end
 
-  defp start_fetching do
+  defp start_fetching(pid) do
     HTTPoison.start()
-    Shell.info("=== AirTr ===")
-    
+    Shell.info("=== AirTr === #{inspect(pid)}")
+
     case Myclient.Client.get(@adbs_url, @headers) do
-      {200, body} -> convert_to_map(body)
+      {200, body} -> convert_to_map(body, pid)
     end
 
+    Shell.info("Size: #{Stack.size(pid)}")
+
     Process.sleep(5000)
-    start_fetching()
+    start_fetching(pid)
   end
 end
